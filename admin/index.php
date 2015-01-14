d5a0de61bea5271e46e4bb1aac11ab1aafa6472a
Josh Watzman <jwatzman@jwatzman.org>
2015-01-13 22:16:25 -0800

    Deal with new rehearsal column

--- admin/index.php
+++ admin/index.php
@@ -4,11 +4,16 @@ require_once '../lib/lib.php';
 
 $new_party_name = idx($_POST, 'new_party_name');
 if (strlen($new_party_name) > 0) {
-	$stmt = db()->prepare('INSERT INTO parties (name) VALUES (:name)');
+	$stmt = db()->prepare(
+		'INSERT INTO parties (name, rehearsal_invited)'
+		.' VALUES (:name, :rehearsal)'
+	);
 	$stmt->bindParam(':name', $new_party_name);
+	$rehearsal = (bool)idx($_POST, 'rehearsal', false);
+	$stmt->bindParam(':rehearsal', $rehearsal);
 	$stmt->execute();
 }
 
-$parties = db()->query('SELECT id, name, comment FROM parties');
+$parties = db()->query('SELECT id, name, comment, rehearsal_invited FROM parties');
 
 echo t()->render('admin/index.html', array('parties' => $parties));
