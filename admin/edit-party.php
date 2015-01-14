ef3ffe2193204244edd6b1b2ad41f9c7578aeff1
Josh Watzman <jwatzman@jwatzman.org>
2015-01-13 23:37:37 -0800

    Add party members

--- admin/edit-party.php
+++ admin/edit-party.php
@@ -15,4 +15,29 @@ if (($party = $stmt->fetch()) === false) {
 	return;
 }
 $party_name = $party['name'];
-echo $party_name;
+
+$new_guest_name = idx($_POST, 'new_guest_name');
+if (strlen($new_guest_name) > 0) {
+	$stmt = db()->prepare(
+		'INSERT INTO guests (name, party_id, is_plus_one)'
+		.' VALUES (:name, :party_id, :is_plus_one)'
+	);
+	$stmt->bindParam(':name', $new_guest_name);
+	$stmt->bindParam(':party_id', $id);
+	$is_plus_one = (bool)idx($_POST, 'plus_one', false);
+	$stmt->bindParam(':is_plus_one', $is_plus_one);
+	$stmt->execute();
+}
+
+$stmt = db()->prepare('SELECT name, is_plus_one, response, rehearsal_response FROM guests WHERE party_id = ?');
+$stmt->execute(array($id));
+$parties = $stmt->fetchAll();
+
+echo t()->render(
+	'admin/edit-party.html',
+	array(
+		'id' => $id,
+		'party_name' => $party_name,
+		'guests' => $parties,
+	)
+);
