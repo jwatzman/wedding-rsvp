57946ca0428541372614e2c36e37a3d48e1c83b7
Josh Watzman <jwatzman@jwatzman.org>
2015-01-15 23:33:01 -0800

    Add akey

--- admin/index.php
+++ admin/index.php
@@ -5,12 +5,20 @@ require_once '../lib/lib.php';
 $new_party_name = idx($_POST, 'new_party_name');
 if (strlen($new_party_name) > 0) {
 	$stmt = db()->prepare(
-		'INSERT INTO parties (name, rehearsal_invited)'
-		.' VALUES (:name, :rehearsal)'
+		'INSERT INTO parties (name, akey, rehearsal_invited)'
+		.' VALUES (:name, :akey, :rehearsal)'
 	);
+
 	$stmt->bindParam(':name', $new_party_name);
+
+	// This is a terrible way to generate a random string, but security isn't a
+	// huge concern, just preventing stupid scraping.
+	$akey = substr(md5(mt_rand()), 0, 4);
+	$stmt->bindParam(':akey', $akey);
+
 	$rehearsal = (bool)idx($_POST, 'rehearsal', false);
 	$stmt->bindParam(':rehearsal', $rehearsal);
+
 	$stmt->execute();
 }
 
