532a88d04b8b7405023708d5a0892f40f7461f3b
Josh Watzman <jwatzman@jwatzman.org>
2015-01-15 23:20:57 -0800

    Tweak DB options

--- lib/db.php
+++ lib/db.php
@@ -4,11 +4,16 @@ function db() {
 	static $db = null;
 	if (!$db) {
 		$db = new PDO(
-			'mysql:host='.DB_HOST.';dbname='.DB_NAME,
+			'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8',
 			DB_USER,
 			DB_PASS
 		);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		$db->setAttribute(
+			PDO::MYSQL_ATTR_INIT_COMMAND,
+			'SET sql_mode="STRICT_ALL_TABLES"'
+		);
 	}
 
 	return $db;
