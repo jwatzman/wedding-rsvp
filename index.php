ada97f4c86d876680639d35a2a0044f05a1d9e54
Josh Watzman <jwatzman@jwatzman.org>
2015-02-07 00:02:18 -0800

    basic password when searching

--- index.php
+++ index.php
@@ -5,8 +5,11 @@ require_once 'lib/lib.php';
 function main__index(): void {
 	$guests = null;
 
-	$search_name = idx($_GET, 'name');
-	if (strlen($search_name) > 1) {
+	$password = idx($_GET, 'password');
+	$password_valid = $password === RSVP_PASS;
+
+	$name = idx($_GET, 'name');
+	if ($password_valid && strlen($name) > 1) {
 		$stmt = db()->prepare(
 			'SELECT guests.name, guests.party_id, parties.akey FROM guests'
 			.' INNER JOIN parties ON guests.party_id = parties.id'
@@ -16,7 +19,7 @@ function main__index(): void {
 		$search_name = str_replace(
 			array('=', '_', '%'),
 			array('==', '=_', '=%'),
-			$search_name
+			$name
 		);
 		$search_name = '%'.$search_name.'%';
 		$stmt->bindParam(':search_name', $search_name);
@@ -24,7 +27,15 @@ function main__index(): void {
 		$guests = $stmt->fetchAll();
 	}
 
-	echo t()->render('index.html', array('guests' => $guests));
+	echo t()->render(
+		'index.html',
+		array(
+			'guests' => $guests,
+			'name' => $name,
+			'password' => $password,
+			'password_valid' => $password_valid,
+		)
+	);
 }
 
 main__index();
