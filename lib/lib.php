3cbff97c56cbc8004f8fceab12f060704304ab1d
Josh Watzman <jwatzman@jwatzman.org>
2015-04-08 20:49:26 -0700

    Log everything

--- lib/lib.php
+++ lib/lib.php
@@ -3,7 +3,20 @@
 error_reporting(E_ALL);
 ini_set('default_charset', 'utf-8');
 
-// TODO log everything
+function log_request(): void {
+	$msg = $_SERVER['SCRIPT_NAME'];
+	$msg .= ' '.$_SERVER['REMOTE_ADDR'].' ';
+	$msg .= ' GET: ';
+	foreach ($_GET as $k => $v) {
+		$msg .= $k.'='.$v.' ';
+	}
+	$msg .= 'POST: ';
+	foreach ($_POST as $k => $v) {
+		$msg .= $k.'='.$v.' ';
+	}
+	error_log($msg);
+}
+log_request();
 
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/twig.php';
