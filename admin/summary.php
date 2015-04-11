242e83215adfd41dadb8aa2c524cc29dbb2361f5
Josh Watzman <jwatzman@jwatzman.org>
2015-04-11 10:24:06 -0700

    Tweak summary display

--- admin/summary.php
+++ admin/summary.php
@@ -8,12 +8,39 @@ function main__admin_summary(): void {
 	$guests = db()->query(
 		'SELECT'
 		.' guests.name, guests.response, guests.rehearsal_response,'
-		.' parties.rehearsal_invited'
+		.' guests.is_plus_one, guests.party_id, parties.rehearsal_invited'
 		.' FROM guests'
 		.' INNER JOIN parties ON guests.party_id = parties.id'
 	);
 
-	echo t()->render('admin/summary.html', array('guests' => $guests));
+	$guests_by_party = array();
+	$total_guests = 0;
+	$total_responses = 0;
+	$total_attending = 0;
+
+	foreach ($guests as $guest) {
+		$guests_by_party[$guest['party_id']][] = $guest;
+		$total_guests++;
+
+		$response = $guest['response'];
+		if ($response !== null) {
+			$total_responses++;
+
+			if ($response) {
+				$total_attending++;
+			}
+		}
+	}
+
+	echo t()->render(
+		'admin/summary.html',
+		array(
+			'guests_by_party' => $guests_by_party,
+			'total_guests' => $total_guests,
+			'total_responses' => $total_responses,
+			'total_attending' => $total_attending,
+		)
+	);
 }
 
 main__admin_summary();
